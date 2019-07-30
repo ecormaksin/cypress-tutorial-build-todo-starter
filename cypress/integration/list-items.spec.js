8d4cc5c84defc752a7e93551da88142cdc20375f
vagrant <vagrant@dummy.com>
2019-07-30 23:08:04 +0900

    5. Todo item behavior 完了したタスクを絞り込むテストの追加

---
+++ cypress/integration/list-items.spec.js
@@ -0,0 +1,14 @@
+describe('List items', () => {
+    beforeEach(() => {
+        cy.seedAndVisit()
+    })
+
+    it.only('properly displays completed items', () => {
+        cy.get('.todo-list li')
+            .filter('.completed')
+            .should('have.length', 1)
+            .and('contain', 'Eggs')
+            .find('.toggle')
+            .should('be.checked')
+    })
+})
