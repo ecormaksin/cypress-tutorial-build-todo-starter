8f1975b4490e07dd033873bf8f90cc377b64eea7
vagrant <vagrant@dummy.com>
2019-07-30 23:15:05 +0900

    5. Todo item behavior 未完了のタスク数をアサートするテストを追加

--- cypress/integration/list-items.spec.js
+++ cypress/integration/list-items.spec.js
@@ -3,12 +3,17 @@ describe('List items', () => {
         cy.seedAndVisit()
     })
 
-    it.only('properly displays completed items', () => {
+    it('properly displays completed items', () => {
         cy.get('.todo-list li')
             .filter('.completed')
             .should('have.length', 1)
             .and('contain', 'Eggs')
             .find('.toggle')
             .should('be.checked')
+    })
+
+    it.only('Shows remaining todos in the footer', () => {
+        cy.get('.todo-count')
+            .should('contain', 3)
     })
 })
