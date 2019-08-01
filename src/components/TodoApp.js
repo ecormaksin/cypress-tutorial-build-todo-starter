ba9fc245e52e45a69943ab8b5321e62d94b6906a
vagrant <vagrant@dummy.com>
2019-08-01 21:24:04 +0900

    6. Toggling and debugging 更新時に配列の要素を置換するよう修正

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -44,15 +44,9 @@ export default class TodoApp extends Component {
     }
     updateTodo(updated)
       .then(({data}) => {
-        debugger
-        const targetIndex = this.state.todos.findIndex(
-          t => t.id === data.id
+        const todos = this.state.todos.map(
+          t => t.id === data.id ? data : t
         )
-        const todos = [
-          ...this.state.todos.slice(0, targetIndex),
-          data,
-          ...this.state.todos.slice(targetIndex + 1)
-        ]
         this.setState({todos: todos})
       })
   }
