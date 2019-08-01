33676410a1392c81419fc175f4155f570fdaf514
vagrant <vagrant@dummy.com>
2019-08-01 21:49:19 +0900

    7. Filters and data-driven tests 複数（3件）残っている場合のテスト
    ※フィクスチャが3件未完了なので実装なしでgreenになる。

--- cypress/integration/footer.spec.js
+++ cypress/integration/footer.spec.js
@@ -1,9 +1,18 @@
 describe('', () => {
     context('with a single todo', () => {
-        it.only('display a singular todo in count', () => {
+        it('displays a singular todo in count', () => {
             cy.seedAndVisit([{id: 1, name: 'Buy milk', isComplete: false}])
             cy.get('.todo-count')
                 .should('contain', '1 todo left')
+        })
+    })
+
+    context('with multiple todos', () => {
+        it.only('displays plural todos in count', () => {
+            cy.seedAndVisit()
+
+            cy.get('.todo-count')
+                .should('contain', '3 todos left')
         })
     })
 })
