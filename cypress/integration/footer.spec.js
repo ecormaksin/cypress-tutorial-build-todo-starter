415076fe6eed7703cbdce0dee612b167be8cf455
vagrant <vagrant@dummy.com>
2019-08-02 21:38:59 +0900

    7. Filters and data-driven tests 同じ操作・アサートを別のパターンで行う場合のテストをwrap, eachで1つに統合

--- cypress/integration/footer.spec.js
+++ cypress/integration/footer.spec.js
@@ -17,20 +17,19 @@ describe('', () => {
                 .should('contain', '3 todos left')
         })
 
-        it('Filters to active todos', () => {
-            cy.contains('Active')
-                .click()
-            
-            cy.get('.todo-list li')
-                .should('have.length', 3)
-        })
-
-        it.only('Filters to completed todos', () => {
-            cy.contains('Completed')
-                .click()
-            
-            cy.get('.todo-list li')
-                .should('have.length', 1)
+        it.only('Handles filter links', () => {
+            const filters = [
+                {link: 'Active', expectedLength: 3},
+                {link: 'Completed', expectedLength: 1},
+            ]
+            cy.wrap(filters)
+                .each(filter => {
+                    cy.contains(filter.link)
+                        .click()
+                    
+                    cy.get('.todo-list li')
+                        .should('have.length', filter.expectedLength)
+                })
         })
     })
 })
